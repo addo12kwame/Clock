e6a5ee45e50dbae7b825689bbcc34f10bf78f3f2
kwame <addo12kwame@gmail.com>
2024-06-15 06:41:08 -0700

    clock

---
+++ clock.py
@@ -0,0 +1,14 @@
+import time
+from time import strftime
+from tkinter import Tk, Label
+import threading
+
+class Clock:
+    def __init__(self):
+        self.app = Tk()
+        self.label = Label(self.app)
+
+
+
+
+
